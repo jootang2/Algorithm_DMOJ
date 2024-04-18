e45449413fa2c63bdc146259a8bcbd7a34dd2276
joohwan <joohwan9607@mobility42.io>
2024-04-19 00:44:17 +0900

    Contest3 init

---
+++ src/main/kotlin/COCI06_Contest3/4_Trojke.kt
@@ -0,0 +1,13 @@
+package COCI06_Contest3
+
+/**
+ * https://dmoj.ca/problem/coci06c3p4
+ * 문제
+ *
+ * Input
+ *
+ * Output
+ *
+ * 풀이
+ *
+ */
---
+++ src/main/kotlin/COCI06_Contest3/6_Lista.kt
@@ -0,0 +1,13 @@
+package COCI06_Contest3
+
+/**
+ * https://dmoj.ca/problem/coci06c3p6
+ * 문제
+ *
+ * Input
+ *
+ * Output
+ *
+ * 풀이
+ *
+ */
