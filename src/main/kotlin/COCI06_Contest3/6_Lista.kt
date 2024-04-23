27f6bed96686b6f12120a2557a4fac0f7ab6adcf
joohwan <joohwan9607@mobility42.io>
2024-04-23 11:00:16 +0900

    문제 파악

--- src/main/kotlin/COCI06_Contest3/6_Lista.kt
+++ src/main/kotlin/COCI06_Contest3/6_Lista.kt
@@ -3,6 +3,14 @@ package COCI06_Contest3
 /**
  * https://dmoj.ca/problem/coci06c3p6
  * 문제
+ * Mirko는 생일 선물로 linked list를 받음
+ * list는 N개의 노드가 1번부터 N번까지 있음
+ * 두 가지로 움직일 수 있음
+ * A : 노드 X를 Y의 앞으로
+ * B : 노드 X를 Y의 뒤로
+ *
+ * 가지고 놀다가 각각의 이동을 종이에 기록
+ * Mirko는 최소한으로 움직이면서 초기상태로 움직이고 싶음
  *
  * Input
  *
